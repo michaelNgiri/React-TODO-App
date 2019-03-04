74063360f4bddf7c18dc6a325d67de792d8a91ce
alveis <michaelngiri@gmail.com>
2019-03-04 10:52:15 +0100

    cleanup component structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
+import Projects from './components/projects';  
 
 
 class App extends Component {
+
+	constructor(){
+		super();
+		this.state = {
+			projects: []
+		}
+	}
+
+	componentWillMount(){
+		this.setState({projects:[
+			{
+				title:'business website',
+				category: 'webdesign'
+			},
+			{
+				title:'Social App',
+				category: 'mobile development'
+			},
+			{
+				title:'Ecommerce shopping cart',
+				category: 'web Development'
+			}
+			]})
+	}
 
 	render=()=>{
 		return(
 		<div className="App">
 			<p>Welcome</p>
+			<Projects projects={this.state.projects} />
 		</div>
 		);
 	}
