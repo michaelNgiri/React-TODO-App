d258a6c59397b1df57d2844d02e18a114d3934db
alveis <michaelngiri@gmail.com>
2019-03-04 10:53:34 +0100

    dreate "addProject" component"

---
+++ src/components/addProject.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class AddProjects extends Component {
+
+	render=()=>{ 
+
+		return(
+		<div>
+		{projectItems}
+		</div>
+		);
+	}
+}
+
+
+
+
+export default AddProjects;
